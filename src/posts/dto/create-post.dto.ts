168c6461f014cb0eb118b2aba81a8c0c76ef8b7d
Aestas <hawhawba@gmail.com>
2025-07-10 10:33:28 +0900

    feat: CreatePostDto에서 title과 content의 유효성 검사 메시지 추가

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,15 +1,31 @@
 import { IsString, IsNotEmpty, MinLength, MaxLength } from 'class-validator'
 
 export class CreatePostDto {
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(2)
-  @MaxLength(100)
+  @IsString({
+    message: 'title은 string타입이어야 합니다.',
+  })
+  @IsNotEmpty({
+    message: 'title은 필수 값입니다.',
+  })
+  @MinLength(2, {
+    message: 'title은 최소 2자 이상이어야 합니다.',
+  })
+  @MaxLength(100, {
+    message: 'title은 최대 100자 이하이어야 합니다.',
+  })
   title: string
 
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(10)
-  @MaxLength(1000)
+  @IsString({
+    message: 'content는 string타입이어야 합니다.',
+  })
+  @IsNotEmpty({
+    message: 'content는 필수 값입니다.',
+  })
+  @MinLength(10, {
+    message: 'content는 최소 10자 이상이어야 합니다.',
+  })
+  @MaxLength(1000, {
+    message: 'content는 최대 1000자 이하이어야 합니다.',
+  })
   content: string
 }
